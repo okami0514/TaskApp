e9371dbb3505d7f2478286c23aa864f76ad282e9
okami-ap <y.okami@alphapolis.co.jp>
2024-03-17 12:16:07 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/youichi/okami/taskapp/MainActivity.kt
@@ -0,0 +1,111 @@
+package jp.techacademy.youichi.okami.taskapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.notifications.InitialResults
+import io.realm.kotlin.notifications.UpdatedResults
+import io.realm.kotlin.query.Sort
+import jp.techacademy.youichi.okami.taskapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+
+const val EXTRA_TASK = "jp.techacademy.youichi.okami.taskapp.TASK"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var taskAdapter: TaskAdapter
+    private lateinit var realm: Realm
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
+        }
+
+        // TaskAdapterを生成し、ListViewに設定する
+        taskAdapter = TaskAdapter(this)
+        binding.listView.adapter = taskAdapter
+
+        // ListViewをタップしたときの処理
+        binding.listView.setOnItemClickListener { parent, _, position, _ ->
+            // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
+        }
+
+        // ListViewを長押ししたときの処理
+        binding.listView.setOnItemLongClickListener { parent, _, position, _ ->
+            // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+            builder.setPositiveButton("OK") { _, _ ->
+                realm.writeBlocking {
+                    // タスクのIDに該当するデータを削除する
+                    val tasks = query<Task>("id==${task.id}").find()
+                    tasks.forEach {
+                        delete(it)
+                    }
+                }
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
+            true
+        }
+
+        // Realmデータベースとの接続を開く
+        val config = RealmConfiguration.create(schema = setOf(Task::class))
+        realm = Realm.open(config)
+
+        // Realmからタスクの一覧を取得
+        val tasks = realm.query<Task>().sort("date", Sort.DESCENDING).find()
+
+        // Realmが起動、または更新（追加、変更、削除）時にreloadListViewを実行する
+        CoroutineScope(Dispatchers.Default).launch {
+            tasks.asFlow().collect {
+                when (it) {
+                    // 更新時
+                    is UpdatedResults -> reloadListView(it.list)
+                    // 起動時
+                    is InitialResults -> reloadListView(it.list)
+                    else -> {}
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Realmデータベースとの接続を閉じる
+        realm.close()
+    }
+
+    /**
+     * リストの一覧を更新する
+     */
+    private suspend fun reloadListView(tasks: List<Task>) {
+        withContext(Dispatchers.Main) {
+            taskAdapter.updateTaskList(tasks)
+        }
+    }
+}
